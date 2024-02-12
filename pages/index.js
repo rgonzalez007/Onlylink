19d62949a2c9debb46bf260ed12f642825d2e1ca
rgmosqueda007 <rgmosqueda007@yahoo.com>
2024-02-12 13:16:25 -0600

    ok

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ const PaginaPrincipal = () => {
   return (
     <div>
        
-      <h2><li><a href="https://unrivaled-brigadeiros-6b8f60.netlify.app">
+      <h2><li><a href="https://courageous-mooncake-9cf2cb.netlify.app">
         Ir a App
       </a></li></h2>
     </div>
